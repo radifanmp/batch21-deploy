89cba62ab470095247c23ad28688d513bfd19dab
Radifan Mardhi Prana <43719706+radifanmp@users.noreply.github.com>
2021-03-26 14:10:11 +0700

    Deploy

---
+++ seeders/20200218074316-demo-user.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+   return queryInterface.bulkInsert('users', [{
+    name: 'John Doe',
+    email: 'johnDoe@gmail.com',
+    password: 'kepo',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  }, {
+    name : 'Suhartono',
+    email : 'suhartono@gmail.com',
+    password : 'kepo',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  }]);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+   return queryInterface.bulkDelete('users', null, {});
+  }
+};
